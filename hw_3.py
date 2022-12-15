4c404ffe913d02d8012ccbdf7b782ad3cc795fcb
Olga Zhivaeva <olga.zhiv@yandex.ru>
2022-12-15 23:30:40 +0300

    Change added

--- hw_3.py
+++ hw_3.py
@@ -6,7 +6,7 @@ for file in files:
        res = f.readlines()
        lines_dict[file] = res
        len_dict[file] = len(res)
-sort_len = sorted(len_dict, key=lines_dict.get, reverse=True)
+sort_len = sorted(len_dict, key=len_dict.get)
 with open('Res.txt', 'at', encoding='utf-8') as f:
     for file in sort_len:
         f.write(f'{file}\n')
