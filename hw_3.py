7a40532ff92532badeb14141966cc29b48e3e1ab
Olga Zhivaeva <olga.zhiv@yandex.ru>
2022-12-15 23:16:14 +0300

    Initial commit

---
+++ hw_3.py
@@ -0,0 +1,16 @@
+files = ['1.txt', '2.txt', '3.txt']
+len_dict = {}
+lines_dict = {}
+for file in files:
+    with open(file, 'rt', encoding='utf-8') as f:
+       res = f.readlines()
+       lines_dict[file] = res
+       len_dict[file] = len(res)
+sort_len = sorted(len_dict, key=lines_dict.get, reverse=True)
+with open('Res.txt', 'at', encoding='utf-8') as f:
+    for file in sort_len:
+        f.write(f'{file}\n')
+        f.write(f'{str(len_dict[file])}\n')
+        for line in lines_dict[file]:
+            f.write(line)
+        f.write('\n')
